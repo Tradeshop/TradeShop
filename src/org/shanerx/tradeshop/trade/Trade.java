a09b47581993216d8a974bd04025e15daafd5f60
Nathan Thor <nathan@thors.org>
2017-07-15 23:49:29 -0700

    Bug fix and Item number support
    Adds support for Item numbers
    Fixes item glitch when trading with different Data (podzol vs dirt)

--- src/org/shanerx/tradeshop/trade/Trade.java
+++ src/org/shanerx/tradeshop/trade/Trade.java
@@ -1,7 +1,5 @@
 package org.shanerx.tradeshop.trade;
 
-import java.awt.Event;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -16,7 +14,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.shanerx.tradeshop.TradeShop;
 import org.shanerx.tradeshop.Utils;
 
@@ -67,50 +64,46 @@ public class Trade extends Utils implements Listener {
 	        
 	        int amount1 = Integer.parseInt(info1[0]);
 	        int amount2 = Integer.parseInt(info2[0]);
-	        String item_name1 = info1[1].toUpperCase();
-	        String item_name2 = info2[1].toUpperCase();
 	        
-			ItemStack item1 = new ItemStack(Enum.valueOf(Material.class, item_name1), amount1); // What the player gets
-	        ItemStack item2 = new ItemStack(Enum.valueOf(Material.class, item_name2), amount2); // What the player pays
+	        String item_name1, item_name2;
 	        
-	        if (!playerInventory.contains(Enum.valueOf(Material.class, item_name2))) {
+	        if(isInt(info1[1]))
+                item_name1 = Material.getMaterial(Integer.parseInt(info1[1])).name();
+            else
+                item_name1 = info1[1].toUpperCase();
+	        
+	        if(isInt(info2[1]))
+                item_name2 = Material.getMaterial(Integer.parseInt(info2[1])).name();
+            else
+                item_name2 = info2[1].toUpperCase();
+	        
+			ItemStack item1 = new ItemStack(Material.getMaterial(item_name1), amount1); // What the player gets
+	        ItemStack item2 = new ItemStack(Material.getMaterial(item_name2), amount2); // What the player pays
+	        
+	        if (!playerInventory.containsAtLeast(item2, amount2)) {
 	        	buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("insufficient-items")
 	        			.replace("{ITEM}", item_name2.toLowerCase()).replace("{AMOUNT}", String.valueOf(amount2))));
 	        	return;
+	        } else {
+	            item1.setData(chestInventory.getItem(chestInventory.first(Material.getMaterial(item_name1))).getData());
+	            item1.setDurability(chestInventory.getItem(chestInventory.first(Material.getMaterial(item_name1))).getDurability());
+	            item1.setItemMeta(chestInventory.getItem(chestInventory.first(Material.getMaterial(item_name1))).getItemMeta());
 	        }
 	        
-	        if (!chestInventory.contains(Enum.valueOf(Material.class, item_name1))) {
+	        if (!chestInventory.containsAtLeast(item1, amount1)) {
 	        	buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("shop-empty")
 	        			.replace("{ITEM}", item_name1.toLowerCase()).replace("{AMOUNT}", String.valueOf(amount1))));
 	        	return;
+	        } else  {
+	            item2.setData(playerInventory.getItem(playerInventory.first(Material.getMaterial(item_name2))).getData());
+	            item2.setDurability(playerInventory.getItem(playerInventory.first(Material.getMaterial(item_name2))).getDurability());
+	            item2.setItemMeta(playerInventory.getItem(playerInventory.first(Material.getMaterial(item_name2))).getItemMeta());
 	        }
 
-	        for (ItemStack i : playerInventory.getContents()) {
-	        	if (i == null) {
-	        		continue;
-	        	}
-	        	if (i.getType() != item2.getType()) {
-	        		continue;
-	        	}
-	        	item2.setItemMeta(i.getItemMeta());
-	        	break;
-	        }
-	        
-	        for (ItemStack i : chestInventory.getContents()) {
-	        	if (i == null) {
-	        		continue;
-	        	}
-	        	if (i.getType() != item1.getType()) {
-	        		continue;
-	        	}
-	        	item1.setItemMeta(i.getItemMeta());
-	        	break;
-	        }
-	        
-	        playerInventory.addItem(item1);
-	        playerInventory.removeItem(item2);
-	        chestInventory.addItem(item2);
-	        chestInventory.removeItem(item1);	        
+            playerInventory.removeItem(item2);
+            chestInventory.removeItem(item1);
+            chestInventory.addItem(item2);
+            playerInventory.addItem(item1);
 	        
 	        String message = plugin.config.getString("on-trade").replace("{AMOUNT1}", String.valueOf(amount1)).replace("{AMOUNT2}", String.valueOf(amount2)).replace("{ITEM1}", item_name1.toLowerCase()).replace("{ITEM2}", item_name2.toLowerCase()).replace("{SELLER}", s.getLine(3));
 	        
@@ -147,4 +140,16 @@ public class Trade extends Utils implements Listener {
 		}
 		
 	}
+
+    //checks to see if a string is an integer, IDK if i use this in this plugin but its here
+    public static boolean isInt(String str)
+    {
+        try{
+            Integer.parseInt(str);
+        }catch(Exception e){
+            return false;
+        }
+        
+        return true;
+    }
 }
