7f5118f9ee6c43b4274a452f313fc3d8dc6a1124
Nathan Thor <nathan@thors.org>
2017-07-16 11:07:13 -0700

    Fixes for bugs and Id support
    - Now pulls items from chest when available, continue to sell after empty

--- src/org/shanerx/tradeshop/itrade/ITrade.java
+++ src/org/shanerx/tradeshop/itrade/ITrade.java
@@ -1,7 +1,5 @@
 package org.shanerx.tradeshop.itrade;
 
-import java.awt.Event;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -16,7 +14,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.shanerx.tradeshop.TradeShop;
 import org.shanerx.tradeshop.Utils;
 
@@ -29,7 +26,8 @@ public class ITrade extends Utils implements Listener {
 	}
 	
 	
-	@EventHandler
+	@SuppressWarnings("deprecation")
+    @EventHandler
 	public void onBlockInteract(PlayerInteractEvent e) {
 		
 		Player buyer = e.getPlayer();
@@ -58,92 +56,155 @@ public class ITrade extends Utils implements Listener {
 			Chest chest = (Chest) chestState;
 			
 			Inventory chestInventory = chest.getInventory();
-			Inventory playerInventory = buyer.getInventory();
-			
-		    String line1 = s.getLine(1);
-	        String line2 = s.getLine(2);
-	        String[] info1 = line1.split(" ");
-	        String[] info2 = line2.split(" ");
-	        
-	        int amount1 = Integer.parseInt(info1[0]);
-	        int amount2 = Integer.parseInt(info2[0]);
-	        String item_name1 = info1[1].toUpperCase();
-	        String item_name2 = info2[1].toUpperCase();
-	        
-			ItemStack item1 = new ItemStack(Enum.valueOf(Material.class, item_name1), amount1); // What the player gets
-	        ItemStack item2 = new ItemStack(Enum.valueOf(Material.class, item_name2), amount2); // What the player pays
-	        
-	        if (!playerInventory.contains(Enum.valueOf(Material.class, item_name2))) {
-	        	buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("insufficient-items")
-	        			.replace("{ITEM}", item_name2.toLowerCase()).replace("{AMOUNT}", String.valueOf(amount2))));
-	        	return;
-	        }
-	        
-	        if (!chestInventory.contains(Enum.valueOf(Material.class, item_name1))) {
-	        	buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("shop-empty")
-	        			.replace("{ITEM}", item_name1.toLowerCase()).replace("{AMOUNT}", String.valueOf(amount1))));
-	        	return;
-	        }
+            Inventory playerInventory = buyer.getInventory();
+            
+            String line1 = s.getLine(1);
+            String line2 = s.getLine(2);
+            String[] info1 = line1.split(" ");
+            String[] info2 = line2.split(" ");
+            
+            int amount1 = Integer.parseInt(info1[0]);
+            int amount2 = Integer.parseInt(info2[0]);
+            
+            String item_name1, item_name2;
+            
+            if(isInt(info1[1]))
+                item_name1 = Material.getMaterial(Integer.parseInt(info1[1])).name();
+            else
+                item_name1 = info1[1].toUpperCase();
+            
+            if(isInt(info2[1]))
+                item_name2 = Material.getMaterial(Integer.parseInt(info2[1])).name();
+            else
+                item_name2 = info2[1].toUpperCase();
+            
+            ItemStack item1 = new ItemStack(Material.getMaterial(item_name1), amount1); // What the player gets
+            ItemStack item2 = new ItemStack(Material.getMaterial(item_name2), amount2); // What the player pays
+            boolean item1check = false, item2check = false;
+            
+            if (!containsAtLeast(playerInventory, item2.getType(), amount2)) {
+                buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("insufficient-items")
+                        .replace("{ITEM}", item_name2.toLowerCase()).replace("{AMOUNT}", String.valueOf(amount2))));
+                return;
+            } else {
+                for(ItemStack i : playerInventory.getContents())
+                {
+                    if(i != null)
+                    {
+                        if(i.getType() == item2.getType())
+                        {
+                            if(i.getAmount() >= amount2)
+                            {
+                                item2.setData(i.getData());
+                                item2.setDurability(i.getDurability());
+                                item2.setItemMeta(i.getItemMeta());
+                                item2check = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if (containsAtLeast(chestInventory, item1.getType(), amount1)) {
+                for(ItemStack i : chestInventory.getContents())
+                {
+                    if(i != null)
+                    {
+                        if(i.getType() == item1.getType())
+                        {
+                            if(i.getAmount() >= amount1)
+                            {
+                                item1.setData(i.getData());
+                                item1.setDurability(i.getDurability());
+                                item1.setItemMeta(i.getItemMeta());
+                                item1check = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            if(item2check)
+            {
+                if(item1check)
+                    chestInventory.removeItem(item1);
+                
+                playerInventory.removeItem(item2);
+                chestInventory.addItem(item2);
+                playerInventory.addItem(item1);
+            }
+            else if(!item2check)
+            {
+                buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("full-amount")
+                .replace("{ITEM}", item_name2.toLowerCase()).replace("{AMOUNT}", String.valueOf(amount2)))); 
+                return;
+            }
+            
+            String message = plugin.config.getString("on-trade").replace("{AMOUNT1}", String.valueOf(amount1)).replace("{AMOUNT2}", String.valueOf(amount2)).replace("{ITEM1}", item_name1.toLowerCase()).replace("{ITEM2}", item_name2.toLowerCase()).replace("{SELLER}", s.getLine(3));
+            
+            buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + message));
+            return;
+        }
+        
+        else if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
+            Sign s;
+            try {
+                s = (Sign) e.getClickedBlock().getState();
+            } catch (Exception ex) {
+                return;
+            }
+            if (! "[iTrade]".equalsIgnoreCase(ChatColor.stripColor(s.getLine(0)))) {
+                return;
+            }
+            
+            try {
+                String line1 = s.getLine(1);
+                String line2 = s.getLine(2);
+                String[] info1 = line1.split(" ");
+                String[] info2 = line2.split(" ");
+                int amount1 = Integer.parseInt(info1[0]);
+                int amount2 = Integer.parseInt(info2[0]);
+                String item_name1 = info1[1].toUpperCase();
+                String item_name2 = info2[1].toUpperCase();
+                
+                buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("confirm-trade").replace("{AMOUNT1}", String.valueOf(amount1)).replace("{AMOUNT2}", String.valueOf(amount2)).replace("{ITEM1}", item_name1.toLowerCase()).replace("{ITEM2}", item_name2.toLowerCase())));
+            } catch (Exception ex) {
+                return;
+            }
 
-	        for (ItemStack i : playerInventory.getContents()) {
-	        	if (i == null) {
-	        		continue;
-	        	}
-	        	if (i.getType() != item2.getType()) {
-	        		continue;
-	        	}
-	        	item2.setItemMeta(i.getItemMeta());
-	        	break;
-	        }
-	        
-	        for (ItemStack i : chestInventory.getContents()) {
-	        	if (i == null) {
-	        		continue;
-	        	}
-	        	if (i.getType() != item1.getType()) {
-	        		continue;
-	        	}
-	        	item1.setItemMeta(i.getItemMeta());
-	        	break;
-	        }
-	        
-	        playerInventory.addItem(item1);
-	        playerInventory.removeItem(item2);	        
-	        
-	        String message = plugin.config.getString("on-trade").replace("{AMOUNT1}", String.valueOf(amount1)).replace("{AMOUNT2}", String.valueOf(amount2)).replace("{ITEM1}", item_name1.toLowerCase()).replace("{ITEM2}", item_name2.toLowerCase()).replace("{SELLER}", s.getLine(3));
-	        
-	        buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + message));
-	        return;
-		}
-		
-		else if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
-			Sign s;
-			try {
-				s = (Sign) e.getClickedBlock().getState();
-			} catch (Exception ex) {
-				return;
-			}
-			if (! "[iTrade]".equalsIgnoreCase(ChatColor.stripColor(s.getLine(0)))) {
-				return;
-			}
-			
-			try {
-			    String line1 = s.getLine(1);
-		        String line2 = s.getLine(2);
-		        String[] info1 = line1.split(" ");
-		        String[] info2 = line2.split(" ");
-		        int amount1 = Integer.parseInt(info1[0]);
-		        int amount2 = Integer.parseInt(info2[0]);
-		        String item_name1 = info1[1].toUpperCase();
-		        String item_name2 = info2[1].toUpperCase();
-		        
-	        	buyer.sendMessage(ChatColor.translateAlternateColorCodes('&', getPrefix() + plugin.config.getString("confirm-trade").replace("{AMOUNT1}", String.valueOf(amount1)).replace("{AMOUNT2}", String.valueOf(amount2)).replace("{ITEM1}", item_name1.toLowerCase()).replace("{ITEM2}", item_name2.toLowerCase())));
+        }
+        
+    }
+    
+    public static boolean containsAtLeast(Inventory inv, Material mat, int amt)
+    {
+        int count = 0;
+        for(ItemStack itm : inv.getContents())
+        {
+            if(itm != null)
+                if(itm.getType() == mat)
+                {
+                    count += itm.getAmount();
+                }
+                
+            if(count >= amt)
+                return true;
+        }
+        
+        return false;
+    }
 
-			} catch (Exception ex) {
-				return;
-			}
-			
-		}
-		
-	}
+    //checks to see if a string is an integer, IDK if i use this in this plugin but its here
+    public static boolean isInt(String str)
+    {
+        try{
+            Integer.parseInt(str);
+        }catch(Exception e){
+            return false;
+        }
+        
+        return true;
+    }
 }
