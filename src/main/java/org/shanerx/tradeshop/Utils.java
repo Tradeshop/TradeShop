232d9b0e20279af496d9700d582a3e99c11527bb
Nathan Thor <nathan@thors.org>
2017-07-19 02:33:07 -0700

    Clean up & Bug fix
    Fixes data value bug 
    Fixes full shop bug

--- src/main/java/org/shanerx/tradeshop/Utils.java
+++ src/main/java/org/shanerx/tradeshop/Utils.java
@@ -2,12 +2,11 @@ package org.shanerx.tradeshop;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
-
-import java.util.ArrayList;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class Utils {
 
@@ -70,4 +69,47 @@ public class Utils {
 			return false;
 		}
 	}
+
+	public boolean canFit(Inventory inv, ItemStack itm, int amt) 
+	{
+	    int count = 0;
+	    if(inv.firstEmpty() >= 0)
+	        return true;
+	    for (ItemStack i : inv.getContents()) 
+	    {
+	        if (i != null) {
+	            if (i.getType() == itm.getType() && i.getData() == itm.getData() && i.getDurability() == itm.getDurability() && i.getItemMeta() == itm.getItemMeta()) {
+	                count += i.getAmount();
+
+	                Bukkit.getServer().broadcastMessage("For Count: " + count);//Debug #Remove
+	            }
+	        }
+	    }
+
+	    while(count >= itm.getMaxStackSize())
+	    {
+	        Bukkit.getServer().broadcastMessage("While Count: " + count);//Debug #Remove
+	        count -= itm.getMaxStackSize();
+	    }
+
+        Bukkit.getServer().broadcastMessage("Count + Amt: " + (count + amt) + "    StackSize: " + itm.getMaxStackSize()); //Debug #Remove
+        if(count == 0)
+            return false;
+        else
+            return count + amt <= itm.getMaxStackSize();
+	}
+	
+    public boolean containsAtLeast(Inventory inv, Material mat, int amt) {
+        int count = 0;
+        for (ItemStack itm : inv.getContents()) {
+            if (itm != null) {
+                if (itm.getType() == mat) {
+                    count += itm.getAmount();
+                }
+            }
+        }
+        return count >= amt;
+    }
+    
+    
 }
