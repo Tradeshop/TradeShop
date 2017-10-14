2b8aee1822203c978ca30b2ed365b4d2ccff59a4
Sparkling Comet <apps@shanerx.org>
2017-10-14 17:34:07 +0200

    Create Message.java

---
+++ src/main/java/org/shanerx/tradeshop/Message.java
@@ -0,0 +1,20 @@
+package org.shanerx.tradeshop;
+
+import org.bukkit.ChatColor;
+
+public enum Message {
+    
+    ;
+    
+    @Override
+    public String toString() {
+        return colour(plugin.getMessages().get(name().toLowerCase()));
+    }
+    
+    private static TradeShop plugin = (TradeShop) Bukkit.getPluginManager().getPlugin("TradeShop");
+    public static final char COLOUR_CHAR = '&';
+    
+    public static String colour(String x) {
+        return ChatColor.translateAlternateColorCodes(COLOUR_CHAR, x);
+    }
+}
