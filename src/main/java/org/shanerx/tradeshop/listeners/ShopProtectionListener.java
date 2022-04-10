ddd0bbcc2df15bf7ab563e3c8748234664d0cff1
KillerOfPie <nathan@thors.org>
2022-04-10 13:47:02 -0700

    Bug Fixes - Fix 2 for Shop status not updating when hoppers push into a shop chest.

--- src/main/java/org/shanerx/tradeshop/listeners/ShopProtectionListener.java
+++ src/main/java/org/shanerx/tradeshop/listeners/ShopProtectionListener.java
@@ -139,6 +139,7 @@ public class ShopProtectionListener extends Utils implements Listener {
         plugin.getListManager().addSkippableHopper(event.getInitiator().getLocation(), hopperEvent.isForbidden());
         debugger.log("ShopProtectionListener: (TSAF) HopperEvent isForbidden: " + hopperEvent.isForbidden(), DebugLevels.PROTECTION);
         shop.updateStatus();
+        shop.updateSign();
         shop.saveShop();
     }
 
